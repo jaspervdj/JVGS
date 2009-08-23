5c9a265a21f0ef35aa004bb18a4cbaa27e68e950
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-08-23 11:53:46 +0200

    Added QuadTree bug when no objects available.

--- src/math/QuadTree.cpp
+++ src/math/QuadTree.cpp
@@ -15,40 +15,49 @@ namespace jvgs
             /* Initialize bounding box. */
             Vector2D topLeft, bottomRight;
             vector<BoundedObject*>::iterator iterator = objects->begin();
-            topLeft = (*iterator)->getBoundingBox()->getTopLeft();
-            bottomRight = (*iterator)->getBoundingBox()->getBottomRight();
-            
-            /* Expand bounding box. */
-            for(iterator++; iterator != objects->end(); iterator++) {
-                BoundingBox *boundingBox = (*iterator)->getBoundingBox();
+            if(objects->begin() != objects->end()) {
 
-                if(boundingBox->getTopLeft().getX() < topLeft.getX())
-                    topLeft.setX(boundingBox->getTopLeft().getX());
-                if(boundingBox->getBottomRight().getX() > bottomRight.getX())
-                    bottomRight.setX(boundingBox->getBottomRight().getX());
+                topLeft = (*iterator)->getBoundingBox()->getTopLeft();
+                bottomRight = (*iterator)->getBoundingBox()->getBottomRight();
+                
+                /* Expand bounding box. */
+                for(iterator++; iterator != objects->end(); iterator++) {
+                    BoundingBox *boundingBox = (*iterator)->getBoundingBox();
 
-                if(boundingBox->getTopLeft().getY() < topLeft.getY())
-                    topLeft.setY(boundingBox->getTopLeft().getY());
-                if(boundingBox->getBottomRight().getY() > bottomRight.getY())
-                    bottomRight.setY(boundingBox->getBottomRight().getY());
-            }
+                    if(boundingBox->getTopLeft().getX() < topLeft.getX())
+                        topLeft.setX(boundingBox->getTopLeft().getX());
+                    if(boundingBox->getBottomRight().getX() > bottomRight.getX())
+                        bottomRight.setX(boundingBox->getBottomRight().getX());
 
-            root = new QuadTreeNode(new BoundingBox(topLeft, bottomRight));
-            for(iterator = objects->begin(); iterator != objects->end();
-                    iterator++) {
-                root->addObject(*iterator);
+                    if(boundingBox->getTopLeft().getY() < topLeft.getY())
+                        topLeft.setY(boundingBox->getTopLeft().getY());
+                    if(boundingBox->getBottomRight().getY() > bottomRight.getY())
+                        bottomRight.setY(boundingBox->getBottomRight().getY());
+                }
+
+                root = new QuadTreeNode(new BoundingBox(topLeft, bottomRight));
+                for(iterator = objects->begin(); iterator != objects->end();
+                        iterator++) {
+                    root->addObject(*iterator);
+                }
+
+            /* No objects. */
+            } else {
+                root = 0;
             }
         }
 
         QuadTree::~QuadTree()
         {
-            delete root;
+            if(root)
+                delete root;
         }
 
         void QuadTree::findObjects(BoundingBox *boundingBox,
                 std::vector<BoundedObject*> *result) const
         {
-            root->findObjects(boundingBox, result);
+            if(root)
+                root->findObjects(boundingBox, result);
         }
     }
 }
