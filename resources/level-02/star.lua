4f58dbb27fe7cc65ca67fa0ac8e54068b0da6e8e
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-09-07 10:36:53 +0200

    Finished level 2.

---
+++ resources/level-02/star.lua
@@ -0,0 +1,11 @@
+require("resources/effects/effects")
+local event = jvgslua.EntityEvent_getEvent()
+
+local events = {
+    collision = function()
+        common.nextLevel("resources/level-03/level-03.xml")
+    end
+}
+
+local f = events[event:getType()]
+if f then f() end
