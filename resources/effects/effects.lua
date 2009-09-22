e53bdfe8d85d74cba24efd99ccad7fcfd3d0d60a
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-09-22 23:20:32 +0200

    Fine-tuned common kill effect some more.

--- resources/effects/effects.lua
+++ resources/effects/effects.lua
@@ -1,27 +1,29 @@
 require("resources/jlib")
 
 effects = {
-    textEffects = {"Whack!", "Splash!", "Pow!"},
-
     stars = function(position)
         local sm = jvgslua.SketchManager_getInstance()
         local em = jvgslua.EffectManager_getInstance()
-        local star = sm:getSketch("resources/effects/star.svg")
+        local sketches = jlib.map(
+                {"moon", "spiral", "star"},
+                function(e)
+                    return sm:getSketch("resources/effects/" .. e .. ".svg")
+                end)
 
         local gravity = jvgslua.Vector2D(0, 0.003)
 
-        for i = 1, math.random(10, 20) do
-            local velocity = jvgslua.Vector2D_fromPolar(1 + 1 * math.random(),
+        for i = 1, math.random(10, 30) do
+            local velocity = jvgslua.Vector2D_fromPolar(0.1 + 1.9 * math.random(),
                     math.random() * 360)
 
-            particle = jvgslua.Particle(star, 1000 + 3000 * math.random(),
-                    position, velocity, gravity)
+            particle = jvgslua.Particle(jlib.randomItem(sketches),
+                    1000 + 3000 * math.random(), position, velocity, gravity)
             em:addEffect(particle)
         end
     end,
 
     text = function(position, str)
-        str = str or jlib.randomItem(effects.textEffects)
+        str = str or jlib.randomItem{"Whack!", "Splash!", "Pow!"}
         local em = jvgslua.EffectManager_getInstance()
         local e = jvgslua.TextEffect(str, position, 1000 + 1000 * math.random(),
                 0.1 + 0.1 * math.random(), 360 * math.random())
--- resources/jlib.lua
+++ resources/jlib.lua
@@ -1,5 +1,11 @@
 jlib = {
     randomItem = function(list)
         return list[math.random(1, #list)]
+    end,
+
+    map = function(list, f)
+        local result = {}
+        for i, v in ipairs(list) do result[i] = f(v) end
+        return result
     end
 }
