52fecbf9021953b8857894fae3dee18db62917c6
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-09-24 18:11:32 +0200

    Added spikey ball trap.

--- resources/invincible-enemy.lua
+++
@@ -1,7 +0,0 @@
-events.trigger{
-    collision = function(self, event)
-        local collider = event:getCollider()
-        local winner, loser = common.impossibleFight(self, collider)
-        if loser then common.kill(loser) end
-    end
-}
--- resources/spikey-ball/ball-script.lua
+++ resources/spikey-ball/ball-script.lua
@@ -1,7 +1,6 @@
 events.trigger{
     collision = function(self, event)
         local collider = event:getCollider()
-        local winner, loser = common.fight(self, collider)
-        if loser then common.kill(loser) end
+        common.kill(collider, true)
     end
 }
