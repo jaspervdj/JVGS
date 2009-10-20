cc0fa452a76a14660ed2c9019bd37e41683ed27f
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-10-20 22:43:43 +0200

    Added a border to the viewer.

--- util/viewer.lua
+++ util/viewer.lua
@@ -7,6 +7,8 @@ local timeManager = jvgslua.TimeManager_getInstance()
 local inputManager = jvgslua.InputManager_getInstance()
 local sketchManager = jvgslua.SketchManager_getInstance()
 
+local border = jvgslua.Vector2D(10, 10)
+
 -- Check arguments.
 if scriptManager:getNumberOfArguments() < 3 then
     print("Usage: " .. scriptManager:getArgument(0) .. " " ..
@@ -21,13 +23,15 @@ videoManager:setVideoMode(jvgslua.Vector2D(200, 200), "Loading...")
 local sketch = sketchManager:getSketch(scriptManager:getArgument(2))
 
 -- Set video mode.
-videoManager:setVideoMode(sketch:getSize(),
+videoManager:setVideoMode(sketch:getSize() + border * 2,
         scriptManager:getArgument(2) .. " - jvgs file viewer")
 
 -- SinputManagerple main loop.
 while not inputManager:hasQuitEvent() do
     inputManager:update(10)
     videoManager:clear()
+    videoManager:identity()
+    videoManager:translate(border)
     sketch:render()
     videoManager:flip()
     timeManager:sleep(10)
