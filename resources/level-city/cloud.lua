7b1536bd9a47df34aa81c9c7ff423cb45e18abba
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-10-20 22:34:19 +0200

    Finished level-city.

---
+++ resources/level-city/cloud.lua
@@ -0,0 +1,7 @@
+events.trigger{
+    collision = function(self, event)
+        if common.isPlayer(event:getCollider()) then
+            common.nextLevel("resources/level-foo/foo.xml")
+        end
+    end
+}
