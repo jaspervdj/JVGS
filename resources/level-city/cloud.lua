19014182cc52b78ec3f0867305ae4eca2f8a1893
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-10-21 15:07:53 +0200

    Started working on level-planet.

--- resources/level-city/cloud.lua
+++ resources/level-city/cloud.lua
@@ -1,7 +1,7 @@
 events.trigger{
     collision = function(self, event)
         if common.isPlayer(event:getCollider()) then
-            common.nextLevel("resources/level-foo/foo.xml")
+            common.nextLevel("resources/level-planet/level.xml")
         end
     end
 }
