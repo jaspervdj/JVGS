d910d94e47fa9a11c1f046d87f3c318b235e74fa
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-01-07 08:12:01 +0100

    Added command line argument parsing.

--- resources/modules/jlib.lua
+++ resources/modules/jlib.lua
@@ -9,3 +9,23 @@ function map(list, f)
     for i, v in ipairs(list) do result[i] = f(v) end
     return result
 end
+
+-- Parses command line options into a table. For example, {"--width", "600"}
+-- would result into a {["width"] = "600"} table.
+function parseOptions()
+    local scriptManager = jvgslua.ScriptManager_getInstance()
+    local i = 0
+    local options = {}
+    while i + 1 < scriptManager:getNumberOfArguments() do
+        local argument = scriptManager:getArgument(i)
+        if string.find(argument, "^--%a+$") then
+            local key = string.sub(argument, 3)
+            local value = scriptManager:getArgument(i + 1)
+            options[key] = value
+            i = i + 2
+        else
+            i = i + 1
+        end
+    end
+    return options
+end
